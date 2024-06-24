98c8d20233400620fcc34b4e31a67eb3ef823995
Robert Elia <relia@pdx.edu>
2024-06-24 11:33:51 -0700

    pushing temp changes so they can be worked on remotely

--- src/ast.rs
+++ src/ast.rs
@@ -8,10 +8,47 @@ pub struct Ast {
     pub children: Vec<Ast>,
 }
 
+pub trait ToAsm {
+    fn to_asm(&self) -> String;
+}
+
 #[derive(Debug)]
 pub enum TokenType {
+    Program,
+    Expr,
+    Statement,
+    UnaryOp,
+}
+
+#[derive(Debug)]
+pub enum Program {
     AstRoot,
-    Function(String),
-    Body,
-    Expression(u32),
+}
+
+#[derive(Debug)]
+pub enum Expr {
+    Number(u32),
+    FunctionCall(String, Vec<Expr>),
+}
+
+
+#[derive(Debug)]
+pub enum Statement {
+    Expression(Expr),
+    Return(Expr),
+    // Name, Params, Body
+    Function(String, Vec<String>, Vec<Statement>),
+}
+
+#[derive(Debug)]
+pub enum UnaryOp {
+    Negation,
+    LogicalNegation,
+    BitwiseComplement,
+}
+
+impl ToAsm for Program {
+    fn to_asm(&self) -> String {
+        format!(".globl {}\n_main", )
+    }
 }
